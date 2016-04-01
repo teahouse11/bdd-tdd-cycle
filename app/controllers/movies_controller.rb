be1582e3cac04e08b1d1ed5eeec21c81ce18a8f5
teahouse11 <austinktang@gmail.com>
2016-04-01 18:23:54 +0000

    added similar directors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -64,4 +64,9 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  def similar_movies
+    movie=params[:movie]
+    @director=movie.director
+    @movies= Movie.find_all_by_director(@director)
+  end
 end
