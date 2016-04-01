2ed27ba0db7793833e88be1e348610dd2e62157e
teahouse11 <austinktang@gmail.com>
2016-04-01 02:41:57 +0000

    add director

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-if ENV['RAILS_ENV'] == "production"
-  ActiveSupport::Deprecation.silenced = true
-end
+
 # Initialize the rails application
 Rottenpotatoes::Application.initialize!
