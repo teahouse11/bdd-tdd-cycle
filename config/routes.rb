f75de03d82d41abb88335e97477e0ba8a820f365
teahouse11 <austinktang@gmail.com>
2016-04-01 18:55:04 +0000

    added similar directors

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,8 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :movies
-
-  match 'movies/similar_movies' => 'movies#similar_movies', :as => :similar_movies
+  resources :movies do
+    match 'movies/similar_movies' => 'movies#similar_movies', :as => :similar_movies
   # Sample resource route with options:
   #   resources :products do
   #     member do
